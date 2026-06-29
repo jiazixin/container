cb56c916a451e3fc1dd754dca10811afa81f084b
jzx <3201471271@qq.com>
2026-06-30 00:07:44 +0800

    test copy

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+skopeo copy docker://docker.io/library/nginx:latest docker://crpi-rabkw7fs4hdymafj.cn-huhehaote.personal.cr.aliyuncs.com/jlibrary/nginx:latest --dest-creds="子夜黑猫q:118107Jzx."
